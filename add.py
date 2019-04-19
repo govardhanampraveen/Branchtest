3d21b6fc7ac99ef59359c34156f850c105e6b4c8
Praveen <govardhanam.praveen@gmail.com>
2019-04-19 07:17:47 +0100

    add.py file commit v1.0

---
+++ add.py
@@ -0,0 +1,8 @@
+# store input numbers
+num1 = input("Enter First number:")
+num2 = input("Enter Second number:")
+# Add two numbers
+sum = fload(num1) + fload(num2)
+# display the sum
+print('The sum'+sum)
+
